7086505099a67243ca6d2ce409e4d9498b6354ce
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-24 15:17:31 +0900

    feat: SetList data

---
+++ src/data.ts
@@ -0,0 +1,31 @@
+import type { SetList } from "@/types";
+
+export const halfBreakSetList: SetList = {
+  bandName: "Half Break",
+  songs: [
+    {
+      name: "Triptych",
+      singer: "Siam Shade",
+    },
+    {
+      name: "ROCKSTAR",
+      singer: "Ado",
+    },
+    {
+      name: "Smells like teen spirit",
+      singer: "Nirvana",
+    },
+    {
+      name: "Good 4 u",
+      singer: "Olivia Rodrigo",
+    },
+    {
+      name: "못 죽는 기사와 비단요럄",
+      singer: "LUCY",
+    },
+    {
+      name: "Marunouchi Sadistic",
+      singer: "Sheena Ringo",
+    },
+  ],
+};
