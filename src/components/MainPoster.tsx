8141e47103b3fd3225bc44d9b6e98bcbf6f690ca
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-24 16:04:37 +0900

    feat: Scroll for Setlist 문구 추가

--- src/components/MainPoster.tsx
+++ src/components/MainPoster.tsx
@@ -7,11 +7,28 @@ export default function MainPoster() {
   return (
     <div
       className={cn(
-        "w-full h-full max-w-sm flex items-end pb-10 justify-center transition-all duration-300",
+        "w-full h-full max-w-sm flex flex-col items-center py-5 justify-center  transition-all duration-300",
         mode === "band" ? "band-poster" : "vocal-poster"
       )}
     >
-      <ToggleSwitch />
+      <div className="h-[33%]">
+        <span
+          className={cn(
+            "font-semibold text-xl p-2 ",
+            mode === "band"
+              ? "text-white bg-white/30"
+              : "text-black bg-black/30"
+          )}
+        >
+          Scroll for Setlist!
+        </span>
+      </div>
+
+      <div className="h-[33%]"></div>
+
+      <div className="flex flex-col justify-end items-center h-[33%]">
+        <ToggleSwitch />
+      </div>
     </div>
   );
 }
