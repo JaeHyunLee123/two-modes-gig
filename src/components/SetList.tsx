bad549e5d9f37dd6d28973e6fc398c0a85da47db
JaeHyunLee <ljh@JaeHyunLees-MacBook-Air.local>
2025-12-26 15:55:12 +0900

    design: 밴드명 폰트 변경

--- src/components/SetList.tsx
+++ src/components/SetList.tsx
@@ -12,14 +12,16 @@ export default function SetList({ setList }: SetListProps) {
   return (
     <div
       className={cn(
-        "flex flex-col items-center justify-center gap-2 h-screen",
+        "flex flex-col items-center justify-center gap-5",
         mode === "band" ? "text-white" : "text-black"
       )}
     >
-      <span className="font-semibold text-xl">{setList.bandName}</span>
-      {setList.songs.map((song, i) => (
-        <span key={i}>{`${song.singer} - ${song.name}`}</span>
-      ))}
+      <span className="text-4xl italianno-regular">{setList.bandName}</span>
+      <ul className="flex flex-col items-center justify-center gap-2">
+        {setList.songs.map((song, i) => (
+          <li key={i}>{`${song.singer} - ${song.name}`}</li>
+        ))}
+      </ul>
     </div>
   );
 }
